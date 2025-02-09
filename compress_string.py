f0e44a512a136922fb2549cb4f60882d1d1b374e
Omar Fadl <omar.fadlallah1@gmail.com>
2025-02-09 16:42:16 +0000

    Add files via upload

---
+++ compress_string.py
@@ -0,0 +1,22 @@
+word="aabbbbcdffl"
+count=1
+result=[]
+i=0
+
+while i<len(word)-1:
+    if word[i]==word[i+1]:
+        count+=1
+    else:
+        result.append(str(count)+word[i] if count>1 else word[i])
+        count=1
+    i+=1
+
+if (i==len(word)-1 and count==1):
+    result.append(word[i])
+
+    
+print("".join(result))
+    
+
+    
+        
